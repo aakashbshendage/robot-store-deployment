002aaf73b6cbb83ee5427e244ccf89ff3561537c
aakashbshendage <aakashshendage3902@gmail.com>
2025-03-14 16:55:15 +0530

    EKS_TERRAFORM Updated

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "aakashbshendage.hotstarclone-1" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate" # tfstate
+    key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
 }

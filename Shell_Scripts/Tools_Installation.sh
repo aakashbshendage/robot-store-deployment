3a51cfa48b24f683dbb8b66b4ddf5992b87c6777
Aakash Balasaheb Shendage <144535729+aakashbshendage@users.noreply.github.com>
2025-03-31 19:22:27 +0530

    Update Tools_Installation.sh

--- Shell_Scripts/Tools_Installation.sh
+++ Shell_Scripts/Tools_Installation.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
-sudo su
-
 # JDK Installation
-apt update -y
+sudo apt update -y
 wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
 echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
-apt update -y
-apt install temurin-17-jdk -y
+sudo apt update -y
+sudo apt install temurin-17-jdk -y
 /usr/bin/java --version
 
 # Jenkins Installation
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-apt-get update -y
-apt-get install jenkins -y
-systemctl start jenkins
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo systemctl start jenkins
 
 # Docker Installation
 # Add Docker's official GPG key:
-apt-get update
-apt-get install ca-certificates curl gnupg -y
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-usermod -aG docker ubuntu
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -aG docker ubuntu
 newgrp docker

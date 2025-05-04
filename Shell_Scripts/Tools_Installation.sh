68a367e5d5304fd4720b20e9214d56ce017be76b
aakashbshendage <aakashshendage3902@gmail.com>
2025-05-04 23:27:22 +0530

    Updated Jenkinsfile

--- Shell_Scripts/Tools_Installation.sh
+++ Shell_Scripts/Tools_Installation.sh
@@ -30,5 +30,5 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo usermod -aG docker ubuntu
-chown -R docker:docker /var/run/docker.sock
-newgrp docker
+sudo newgrp docker
+sudo chmod 777 /var/run/docker.sock
